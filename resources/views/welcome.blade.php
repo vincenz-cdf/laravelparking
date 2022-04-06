2301234aa89a513999dcd9399b86307b8c22702f
PerThoGon <91068148+PerThoGon@users.noreply.github.com>
2022-04-06 18:56:53 +0200

    xsdfvbgn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,15 +47,15 @@
 										</div>
 										<ul class="actions">
 											@if (Route::has('login'))
-											@auth
-                        						<li><a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a></li>
-											@else
-												<li><a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a></li>
-												@if (Route::has('register'))
-												<li><a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a></li>
+												@auth
+                        							<li><a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a></li>
+												@else
+													<li><a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a></li>
+													@if (Route::has('register'))
+													<li><a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a></li>
+													@endif
+												@endauth
 											@endif
-										@endauth
-										@endif
 										</ul>
 									</form>
 								</section>
